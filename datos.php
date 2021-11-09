905522df00c3c56999c3030935a4d637b4e96293
Daniel <danielpico2000@hotmail.es>
2021-11-09 16:31:17 -0500

    main

--- datos.php
+++ datos.php
@@ -2,6 +2,27 @@
 
 require_once __DIR__ . '/vendor/autoload.php';
 
+$client = new MongoDB\Client(
+    'mongodb+srv://Daniel:12342234@cluster0.y2xfg.mongodb.net/MultimediaS?retryWrites=true&w=majority');
+ 
+$tb=$client->water->Precipitaciones;
+$filter= ['$and'=>
+            [
+                ['ANNO'=>['$eq'=>'1997']],
+                ['ESTACION'=>['$eq'=>'Sutatausa']]
+            ]
+        ];
+ 
+$query = new MongoDB\Driver\Query($filter);
+ 
+$rows = $tb->find($filter);
+$datos= iterator_to_array($rows);
+echo json_encode($datos);
+
+
+/*
+require_once __DIR__ . '/vendor/autoload.php';
+
 
 $client = new MongoDB\Client(
     'mongodb+srv://Daniel:12342234@cluster0.y2xfg.mongodb.net/MultimediaS?retryWrites=true&w=majority');
@@ -17,6 +38,6 @@ $registro=array(
 $resultado=$tb->insertOne($registro);
 echo $resultado->getInsertedCount();
 
-
+*/
 
 ?>
