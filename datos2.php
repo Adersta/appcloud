bf56f22b76f18903380f813bcf017f5fd4837ed8
Daniel <danielpico2000@hotmail.es>
2021-11-17 09:33:19 -0500

    main

---
+++ datos2.php
@@ -0,0 +1,48 @@
+<?php 
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+$a=$_GET['anno'];
+$b=$_GET['provedor'];
+$c=$_GET['estrato'];
+
+$client = new MongoDB\Client(
+    'mongodb+srv://Daniel:12342234@cluster0.y2xfg.mongodb.net/MultimediaS?retryWrites=true&w=majority');
+ 
+$tb=$client->telefonia->telefonia;
+$filter= ['$and'=>
+            [
+                ['AÑO'=>['$eq'=>$a]],
+                ['PROVEEDOR'=>['$eq'=>$b]]
+                ['SEGMENTO'=>['$eq'=>$c]]
+            ]
+        ];
+ 
+$query = new MongoDB\Driver\Query($filter);
+ 
+$rows = $tb->find($filter);
+$datos= iterator_to_array($rows);
+echo json_encode($datos);
+
+
+/*
+require_once __DIR__ . '/vendor/autoload.php';
+
+
+$client = new MongoDB\Client(
+    'mongodb+srv://Daniel:12342234@cluster0.y2xfg.mongodb.net/MultimediaS?retryWrites=true&w=majority');
+
+$tb=$client->MultimediaS->puntaje;
+
+$registro=array(
+    "iduser"=>"1458",
+    "fecha"=>"1/1/2031",
+    "puntaje"=>"1458"
+);
+
+$resultado=$tb->insertOne($registro);
+echo $resultado->getInsertedCount();
+
+*/
+
+?>
